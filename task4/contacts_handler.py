5ace75f8ba8bdef9a689975dc42f02612693e7ab
Yurii Tatach <yu.touch@icloud.com>
2024-05-05 17:55:54 +0300

    Task 4 for HW5. Add decorator for handlers

--- task4/contacts_handler.py
+++ task4/contacts_handler.py
@@ -1,8 +1,12 @@
+from error_handler import input_error
+
+@input_error
 def add_contact(args:list, contacts:dict) -> str:
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+@input_error
 def change_contact(args:list, contacts:dict) -> str:
     name, phone = args
     if name in contacts:
